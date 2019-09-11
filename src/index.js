f0f057cb0865bc109a6f8fb8631af99bdec24d57
101arrowz <arjunbarrett@gmail.com>
2019-09-11 13:17:51 -0700

    Added full PWA functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import { render } from 'preact';
 import App from './App';
 if ('serviceWorker' in navigator) {
   // Uncomment below when prod - annoying for dev
-  // navigator.serviceWorker.register('/sw.js', { scope: '/' });
+  navigator.serviceWorker.register('../public/sw.js');
 }
 render(<App />, document.getElementById('root'));
